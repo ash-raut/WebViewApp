a03473df54bf29cfaa199f5c04c8f4e4f9642ac7
user <ashwiniraut124@gmail.com>
2019-09-09 23:52:44 -0700

    Initial commit

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2;
+     WebView simpleWebView;
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=findViewById(R.id.loadWebPage);
+        b2=findViewById(R.id.loadFromStaticHtml);
+        simpleWebView=findViewById(R.id.simpleWebView);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //define static html
+
+
+                simpleWebView.setWebViewClient(new MyWebViewClient());
+                String url = "https://chandansatyendraprasad.wordpress.com/";
+                simpleWebView.getSettings().setJavaScriptEnabled(true);
+                simpleWebView.loadUrl(url); // load a web page in a web view
+
+
+
+            }
+        });
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            class MyWebViewClient extends WebViewClient {
+            }
+
+            @Override
+            public void onClick(View view) {
+
+
+                String customHtml = "<html><body><h1>Hello, Android App Developer</h1>" +
+                        "<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3>" +
+                        "<p>This is a sample paragraph of static HTML In Web view</p>" +
+                        "</body></html>";
+                simpleWebView.loadData(customHtml, "text/html", "UTF-8"); // load html string data in a web view
+
+
+            }
+        });
+
+
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+}
